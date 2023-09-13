cdd69bbcaea7b4d7833ea491584ff79f404c85d9
gamiken <gamiken@kappa-lab.com>
2023-09-13 16:13:01 +0900

    get location.href

--- components/pageview.go
+++ components/pageview.go
@@ -10,13 +10,15 @@ import (
 
 type PageView struct {
 	vecty.Core
+
+	url         string
 	buttonLabel string
 	message     string
 	messages    []string
 }
 
-func NewPageView() *PageView {
-	return &PageView{buttonLabel: "Clear"}
+func NewPageView(url string) *PageView {
+	return &PageView{buttonLabel: "Clear", url: url}
 }
 
 func (p *PageView) reset() *PageView {
@@ -37,8 +39,10 @@ func (p *PageView) Render() vecty.ComponentOrHTML {
 		),
 		elem.Script(
 			vecty.Text("console.log('Ready...');"),
+			vecty.Text("console.log(location.href);"),
 			vecty.Text("function log(val) {console.log(val)};"),
 		),
+
 		elem.Div(
 			vecty.Markup(
 				vecty.Class("display-2"),
@@ -54,12 +58,16 @@ func (p *PageView) Render() vecty.ComponentOrHTML {
 					event.Submit(func(e *vecty.Event) {
 						p.messages = append(p.messages, p.message)
 						p.message = ""
+						elem.Script(
+							vecty.Text("console.log('done');"),
+						)
 						vecty.Rerender(p)
 					}).PreventDefault(),
 				),
 
 				elem.Input(
 					vecty.Markup(
+						vecty.Attribute("name", "message-input"),
 						prop.Placeholder("Enter Message"),
 						prop.Autofocus(true),
 						prop.Value(p.message),
@@ -72,7 +80,8 @@ func (p *PageView) Render() vecty.ComponentOrHTML {
 		),
 
 		elem.Div(
-			messages...,
+			vecty.Markup(vecty.Attribute("name", "message-list")),
+			elem.OrderedList(messages...),
 		),
 
 		elem.Div(
@@ -87,6 +96,11 @@ func (p *PageView) Render() vecty.ComponentOrHTML {
 					vecty.Text(p.buttonLabel), // initial textarea text.
 				),
 			),
+		),
+
+		elem.Div(
+			elem.Heading4(vecty.Text("URL")),
+			elem.Paragraph(vecty.Text(p.url)),
 		),
 	)
 }
--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"syscall/js"
+
 	"github.com/hexops/vecty"
 	"github.com/kappa-lab/vecty-playground/components"
 )
 
 func main() {
-
+	location := js.Global().Get("location").Get("href")
 	vecty.SetTitle("Mypage")
 
 	vecty.AddStylesheet("https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css")
 
-	vecty.RenderBody(components.NewPageView())
+	vecty.RenderBody(components.NewPageView(fmt.Sprintf("%s", location)))
 
 }
